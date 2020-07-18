3f73b950b7237655d7b96d0c0cf0b31c6e9138ad
Marissa <marissacooterl@gmail.com>
2020-07-17 20:10:19 -0500

    built out auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,17 +1,58 @@
 const router = require('express').Router();
 const bcrypt = require("bcryptjs")
+const Auth = require("./auth-model")
 const restrict = require("./authenticate-middleware")
 
 router.post('/register', async (req, res, next) => {
   try {
     const { username, password } = req.body
+    const user = await Auth.findBy({ username }).first()
+
+    if (user) {
+      return res.status(409).json({
+        message: "Username is already taken"
+      })
+    }
+
+    const newUser = await Auth.add({
+      username,
+      password: await bcrypt.hash(password, 14)
+    })
+
+    res.status(201).json(newUser)
   } catch(err) {
     next(err)
   }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const user = await Auth.findBy({ username }).first()
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      })
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password)
+
+    if (!passwordValid) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      })
+    }
+
+    req.session.user = user
+
+    res.json({
+      message: `Welcome ${user.username}!`
+    })
+
+  } catch(err) {
+    next(err)
+  }
 });
 
 module.exports = router;
