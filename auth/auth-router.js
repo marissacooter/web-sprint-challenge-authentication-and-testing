ab4e9b8857a91c01d92494be6fe3e0a072241528
Marissa <marissacooterl@gmail.com>
2020-07-17 19:52:09 -0500

    created an auth model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs")
+const restrict = require("./authenticate-middleware")
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+  } catch(err) {
+    next(err)
+  }
 });
 
 router.post('/login', (req, res) => {
